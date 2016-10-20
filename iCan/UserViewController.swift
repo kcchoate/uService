a88c894992bae750ba441986617900e46233c40a
Kendrick Choate <kendrickchoate@gmail.com>
2016-10-19 19:04:27 -0500

    Hid the navigation bar in the user view to prevent going back after first login.

--- iCan/UserViewController.swift
+++ iCan/UserViewController.swift
@@ -12,7 +12,7 @@ class UserViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationController?.navigationBar.isHidden = true
         // Do any additional setup after loading the view.
     }
 
